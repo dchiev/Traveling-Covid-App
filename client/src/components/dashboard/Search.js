85ed58e507b68769c9f50968926066c9a396174a
Dan Chiev <danchiev2@Gmail.com>
2021-01-30 13:51:07 -0500

    can now console.log search value

--- client/src/components/dashboard/Search.js
+++ client/src/components/dashboard/Search.js
@@ -1,28 +1,31 @@
 import React from "react";
+class search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.addValue = this.addValue.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+  }
 
-function Search(props) {
-  return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="search">Search:</label>
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for 2-digit a country code"
-          id="search"
-        />
-        <button
-          onClick={props.handleFormSubmit}
-          className="btn btn-primary mt-3"
-        >
-          Search
-        </button>
-      </div>
-    </form>
-  );
+  addValue(evt) {
+    evt.preventDefault();
+    if (this.state.value !== undefined) {
+      console.log("Your input value is: " + this.state.value);
+    }
+  }
+  updateInput(evt) {
+    this.setState({ value: evt.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.addValue}>
+        <input type="text" onChange={this.updateInput} />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
-
-export default Search;
+export default search;
