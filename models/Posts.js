97b6a926e9397ed362597d744a84ef8262a1450c
Dan Chiev <danchiev2@Gmail.com>
2021-02-09 21:33:22 -0500

    passport working

--- models/Posts.js
+++ models/Posts.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostsSchema = new Schema({
+const postsSchema = new Schema({
   country: { type: String, required: true },
   text: { type: String, required: true },
   date: { type: Date, default: Date.now },
