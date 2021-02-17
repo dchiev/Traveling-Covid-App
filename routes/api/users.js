98da6e130751c137f6f6d6b73e30a80067b81bc8
Dan Chiev <danchiev2@Gmail.com>
2021-02-16 21:32:13 -0500

    finally?

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
@@ -83,7 +82,7 @@ router.post("/login", (req, res) => {
         // Sign token
         jwt.sign(
           payload,
-          keys.secretOrKey,
+          process.env.JWT_SECRET,
           {
             expiresIn: 31556926, // 1 year in seconds
           },
