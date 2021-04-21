c439ec42308f959640a6af9b7fb3cee264b4f7a0
Dan Chiev <danchiev2@Gmail.com>
2021-04-21 19:58:24 -0400

    cors update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export default {
   findCountry1: function (countrySearch) {
     const queryURL1 =
-      "https://arcane-woodland-47583.herokuapp.com/https://www.travel-advisory.info/api?countrycode=" +
+      "https://polar-headland-42804.herokuapp.com/https://www.travel-advisory.info/api?countrycode=" +
       countrySearch;
     return axios.get(queryURL1);
   },
   findCountry2: function (countrySearch) {
     const queryURL2 =
-      "https://arcane-woodland-47583.herokuapp.com/https://www.trackcorona.live/api/countries/" +
+      "https://polar-headland-42804.herokuapp.com/https://www.trackcorona.live/api/countries/" +
       countrySearch;
     return axios.get(queryURL2);
   },
